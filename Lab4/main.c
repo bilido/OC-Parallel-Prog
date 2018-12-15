9ea78ede46e7943c3b3aecfc140a6178fda2fa35
Bili Ido <idomohamed@gmail.com>
2018-12-14 19:42:04 -0600

    Update main.c

--- Lab4/main.c
+++ Lab4/main.c
@@ -9,13 +9,74 @@ static const long Num_To_Sort = 1000000000;
 
 // Sequential version of your sort
 // If you're implementing the PSRS algorithm, you may ignore this section
-void sort_s(int *arr) {
 
+void swap(long* valA, long* valB){
+    long temp = *valA;
+    *valA = *valB;
+    *valB = temp;
+}
+
+// QuickSort Algorithm implementation Reference: https://www.geeksforgeeks.org/quick-sort/
+
+/* This function takes last element as pivot, places
+   the pivot element at its correct position in sorted
+    array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right
+   of pivot */
+long partition (int* arr[], int low, int high){
+    // pivot (Element to be placed at right position)
+    long pivot = arr[high];
+    int i = (low - 1); // Index of smaller element
+    for (int j=low; j <= high - 1; j++){
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot){
+            i++;    // increment index of smaller element
+            swap(&arr[i],&arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+
+    return (i + 1);
+}
+
+void quickSort_s(int* arr, int low, int high){
+    if(low < high){
+        /* pi is partitioning index, arr[pi] is now
+           at right place */
+        long pi = partition(arr, low, high);
+
+        quickSort_s(arr, low, pi - 1);  // Before pi
+        quickSort_s(arr, pi + 1, high); // After pi
+    }
+}
+
+void sort_s(int* arr) {
+
+    quickSort_s(arr, 0, Num_To_Sort - 1);
+}
+
+void quickSort_p(int* arr, int low, int high){
+    if(low < high){
+        long pi = partition(arr, low, high);
+
+        #pragma  omp parallel sections
+        {
+            #pragma omp section
+            quickSort_p(arr, low, pi - 1);
+
+            #pragma omp section
+            quickSort_p(arr, pi + 1, high);
+        }
+
+    }
 }
 
 // Parallel version of your sort
 void sort_p(int *arr) {
 
+    long num_to_sort_p = sizeof(arr) / sizeof(arr[0]);
+    quickSort_p(arr, 0, num_to_sort_p - 1);
 }
 
 int main() {
